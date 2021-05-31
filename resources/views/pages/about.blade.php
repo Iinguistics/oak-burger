e728b297f7c5bea49ba70c2ec76f10be4336ea5e
James <Thylinguistics@gmail.com>
2021-05-31 14:50:07 -0700

    ...

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,18 +6,18 @@
 <div id="about-page">
   <div class="content-box">
     <div class="row">
-      <div class="col-md-6">
+      <div class="col">
         <h1 class="mb-3">About Oak Burger</h1>
         <p>I'm baby raw denim taxidermy distillery, shoreditch heirloom quinoa literally church-key hell of. Asymmetrical farm-to-table la croix migas tote bag fanny pack snackwave meditation 90's schlitz tofu taiyaki. Freegan pour-over cold-pressed 90's, raclette yr bitters copper mug. Sustainable poke vexillologist green juice retro woke neutra raclette try-hard meh quinoa cold-pressed.
           Franzen meggings master cleanse jean shorts blog everyday carry. Everyday carry cloud bread fam adaptogen. Affogato asymmetrical mumblecore everyday carry narwhal polaroid. Green juice wolf fixie wayfarers unicorn.
-          Distillery man bun chicharrones man braid. VHS prism fashion axe humblebrag. Green juice vaporware taiyaki gochujang, unicorn XOXO blog hammock master cleanse lo-fi intelligentsia chillwave paleo copper mug. Humblebrag occupy hashtag small batch affogato keffiyeh cardigan flannel synth. Typewriter tacos distillery, put a bird on it jianbing cray 3 wolf moon. La croix 8-bit mustache whatever cardigan drinking vinegar artisan.
         </p>
         <p>I'm baby raw denim taxidermy distillery, shoreditch heirloom quinoa literally church-key hell of. Asymmetrical farm-to-table la croix migas tote bag fanny pack snackwave meditation 90's schlitz tofu taiyaki. Freegan pour-over cold-pressed 90's, raclette yr bitters copper mug. Sustainable poke vexillologist green juice retro woke neutra raclette try-hard meh quinoa cold-pressed.
           Franzen meggings master cleanse jean shorts blog everyday carry. Everyday carry cloud bread fam adaptogen. Affogato asymmetrical mumblecore everyday carry narwhal polaroid. Green juice wolf fixie wayfarers unicorn.
-          Distillery man bun chicharrones man braid. VHS prism fashion axe humblebrag. Green juice vaporware taiyaki gochujang, unicorn XOXO blog hammock master cleanse lo-fi intelligentsia chillwave paleo copper mug. Humblebrag occupy hashtag small batch affogato keffiyeh cardigan flannel synth. Typewriter tacos distillery, put a bird on it jianbing cray 3 wolf moon. La croix 8-bit mustache whatever cardigan drinking vinegar artisan.
         </p>
       </div>
-      <div class="col-md-6">
+    </div>
+    <div class="row">
+      <div class="col">
         <img class="img-fluid" src="img/about-oak-burger.jpg" />
       </div>
     </div>

7c1a67139d0b08f78c4e9b326447fc45081956aa
James <Thylinguistics@gmail.com>
2021-06-01 17:39:53 -0700

    ...

--- resources/views/pages/waitlist.blade.php
+++ resources/views/pages/waitlist.blade.php
@@ -3,7 +3,45 @@
 
 
 @section('content')
-  <div class="">
-    <h1>Waitlist Page</h1>
+<div id="about-page">
+  <div class="content-box">
+    <div class="row">
+      <div class="col-md-6">
+        <h1 class="mb-3">Get on the list</h1>
+        <form>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Example select</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlSelect2">Example multiple select</label>
+            <select multiple class="form-control" id="exampleFormControlSelect2">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
+
+</div>
+  
 @endsection

a1a341bef4a2d0d29861422c865b2e403d81279c
James <Thylinguistics@gmail.com>
2021-06-04 20:19:46 -0700

    ...

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -3,8 +3,26 @@
 
 
 @section('content')
-  <div class="welcome-jumbo">
-    
-    <h1>Menu Page</h1>
+<div id="menu-page">
+  <div class="content-box">
+    <div class="row">
+      <div class="col">
+        <h1 class="mb-3">Menu</h1>
+        <p>I'm baby raw denim taxidermy distillery, shoreditch heirloom quinoa literally church-key hell of. Asymmetrical farm-to-table la croix migas tote bag fanny pack snackwave meditation 90's schlitz tofu taiyaki. Freegan pour-over cold-pressed 90's, raclette yr bitters copper mug. Sustainable poke vexillologist green juice retro woke neutra raclette try-hard meh quinoa cold-pressed.
+          Franzen meggings master cleanse jean shorts blog everyday carry. Everyday carry cloud bread fam adaptogen. Affogato asymmetrical mumblecore everyday carry narwhal polaroid. Green juice wolf fixie wayfarers unicorn.
+        </p>
+        <p>I'm baby raw denim taxidermy distillery, shoreditch heirloom quinoa literally church-key hell of. Asymmetrical farm-to-table la croix migas tote bag fanny pack snackwave meditation 90's schlitz tofu taiyaki. Freegan pour-over cold-pressed 90's, raclette yr bitters copper mug. Sustainable poke vexillologist green juice retro woke neutra raclette try-hard meh quinoa cold-pressed.
+          Franzen meggings master cleanse jean shorts blog everyday carry. Everyday carry cloud bread fam adaptogen. Affogato asymmetrical mumblecore everyday carry narwhal polaroid. Green juice wolf fixie wayfarers unicorn.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <img class="img-fluid" src="img/about-oak-burger.jpg" />
+      </div>
+    </div>
   </div>
+
+</div>
+  
 @endsection

acb78fde4639ba75703dfe9cd80703af1a4ae9c0
James <Thylinguistics@gmail.com>
2021-06-07 13:25:28 -0700

    ...

--- resources/views/menu/single-menu.blade.php
+++ resources/views/menu/single-menu.blade.php
@@ -4,8 +4,51 @@
 
 @section('content')
 <div id="single-menu-page">
+    <div class="row">
+        <div class="offset-md-2 col-md-8">
+            <div class="content-box">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Burgers</h1>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="item"> <h4>Texas Burger</h4>
+                            <span class="price">$8</span>
+                        </div>
+                        <div class="description">
+                            <p>two grilled quarter-pound beef patties, american cheese, shredded lettuce, tomato, red onion, pickles, housemade bark + bite sauce</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="item"> <h4>Texas Burger</h4>
+                            <span class="price">$8</span>
+                        </div>
+                        <div class="description">
+                            <p>two grilled quarter-pound beef patties, american cheese, shredded lettuce, tomato, red onion, pickles, housemade bark + bite sauce</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="item"> <h4>Texas Burger</h4>
+                            <span class="price">$8</span>
+                        </div>
+                        <div class="description">
+                            <p>two grilled quarter-pound beef patties, american cheese, shredded lettuce, tomato, red onion, pickles, housemade bark + bite sauce</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="item"> <h4>Texas Burger</h4>
+                            <span class="price">$8</span>
+                        </div>
+                        <div class="description">
+                            <p>two grilled quarter-pound beef patties, american cheese, shredded lettuce, tomato, red onion, pickles, housemade bark + bite sauce</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 
- testing
 
 </div>
   

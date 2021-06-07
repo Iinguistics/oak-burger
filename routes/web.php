2144211b9aeb7ce542cc62a5959af847413de8fa
James <Thylinguistics@gmail.com>
2021-06-07 15:06:36 -0700

    ...

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
+
+
 Route::get('/menu', function () {
     return view('menu.index');
 });
